45c43fdafa63c8d70ae6865cfae648784c2c9375
Edilbek <edil.talantbekov@gmail.com>
2019-07-10 23:09:17 +0600

    finish

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -7,6 +7,43 @@ class TodayController < ApplicationController
   def five_day
     five_day = ApiXu::FiveDayWeather.new("Bishkek")
     @five_today_apix = five_day.fiveday_weather
-    @five_today_dark = DarkSky::FiveDayWeather.new("42.862,74.557")
+
+    @today_apix = ApiXu::TodayWeather.new("Bishkek")
+    @today_dark = DarkSky::TodayWeather.new("42.862,74.557")
+    
+    @five_day_d = DarkSky::FiveDayWeather.new("42.862,74.557")
+    @five_today_dark = @five_day_d.fiveday_weather
+  end
+
+  def batken_weather
+    @today_apix = ApiXu::TodayWeather.new("Batken")
+    @today_dark = DarkSky::TodayWeather.new("40.06259,70.81939")
+  end
+
+  def talas_weather
+    @today_apix = ApiXu::TodayWeather.new("Talas")
+    @today_dark = DarkSky::TodayWeather.new("42.52277,72.24274")
+  end
+
+  def fiveday_batken_weather
+    five_day = ApiXu::FiveDayWeather.new("Batken")
+    @five_today_apix = five_day.fiveday_weather
+
+    @today_apix = ApiXu::TodayWeather.new("Batken")
+    @today_dark = DarkSky::TodayWeather.new("40.06259,70.81939")
+    
+    @five_day_d = DarkSky::FiveDayWeather.new("40.06259,70.81939")
+    @five_today_dark = @five_day_d.fiveday_weather
+  end
+
+  def fiveday_talas_weather
+    five_day = ApiXu::FiveDayWeather.new("Talas")
+    @five_today_apix = five_day.fiveday_weather
+
+    @today_apix = ApiXu::TodayWeather.new("Talas")
+    @today_dark = DarkSky::TodayWeather.new("42.52277,72.24274")
+    
+    @five_day_d = DarkSky::FiveDayWeather.new("42.52277,72.24274")
+    @five_today_dark = @five_day_d.fiveday_weather
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "today#today"
   get 'five_day', to: :five_day, controller: 'today'
+  get 'talas_weather', to: :talas_weather, controller: 'today'
+  get 'batken_weather', to: :batken_weather, controller: 'today'
+  get 'fiveday_batken_weather', to: :fiveday_batken_weather, controller: 'today'
+  get 'fiveday_talas_weather', to: :fiveday_talas_weather, controller: 'today'
 end
