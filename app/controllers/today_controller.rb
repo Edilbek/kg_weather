630a727d4c03c2a4494b5a4ebde0643a1ed3afaa
Edilbek <edil.talantbekov@gmail.com>
2019-07-10 20:54:07 +0600

    showed current weather

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -1,9 +1,12 @@
 class TodayController < ApplicationController
   def today
     @today_apix = ApiXu::TodayWeather.new("Bishkek")
+    @today_dark = DarkSky::TodayWeather.new("42.862,74.557")
   end
   
   def five_day
-    
+    five_day = ApiXu::FiveDayWeather.new("Bishkek")
+    @five_today_apix = five_day.fiveday_weather
+    @five_today_dark = DarkSky::FiveDayWeather.new("42.862,74.557")
   end
 end
