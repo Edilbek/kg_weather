0c1bfb51ce495061d733c46b00eb69e98152a9b4
Edilbek <edil.talantbekov@gmail.com>
2019-07-14 02:41:02 +0600

    started work with front end

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,10 +1,11 @@
 class WeatherController < ApplicationController
   def forecast
     @city = params[:city]
-    day  = params[:day].to_i
-    @apixu_forecast = WeatherGemKg::Generator.new("#{@city}", day,
+    @day  = params[:day].to_i
+    @cities = ["Bishkek", "Talas", "Batket"]
+    @apixu_forecast = WeatherGemKg::Generator.new("#{@city}", @day,
       WeatherGemKg::Clients::Apixu.new).generate_me
-    @dark_forecast  = WeatherGemKg::Generator.new("#{@city}", day,
+    @dark_forecast  = WeatherGemKg::Generator.new("#{@city}", @day,
       WeatherGemKg::Clients::DarkSky.new).generate_me
   end
 end
